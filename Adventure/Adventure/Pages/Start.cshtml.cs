50301cc3b7dea1c7cafdc6f82faa5355829f3f4e
janreisiegel <jan.reisiegel@pslib.cz>
2020-04-26 17:02:11 +0200

    vytvoření stránek Start a Konec

---
+++ Adventure/Adventure/Pages/Start.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Adventure.Pages
+{
+    public class StartModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
